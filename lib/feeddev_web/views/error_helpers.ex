d7641121e55a535360228866b3792ea4a0300730
Emeric MARTINEAU <11473190+bubulemaster@users.noreply.github.com>
2020-05-22 08:48:46 +0200

    Update pow view for pow 1.0.20

--- lib/feeddev_web/views/error_helpers.ex
+++ lib/feeddev_web/views/error_helpers.ex
@@ -8,15 +8,6 @@ defmodule FeeddevWeb.ErrorHelpers do
   @doc """
   Generates tag for inlined form input errors.
   """
-  def error_tag(form, field) do
-    Enum.map(Keyword.get_values(form.errors, field), fn error ->
-      content_tag(:span, translate_error(error), class: "help-block")
-    end)
-  end
-
-  @doc """
-  Generates tag for inlined form input errors.
-  """
   def error_tag(form, field, f) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
       f.(translate_error(error))
--- lib/feeddev_web/views/pow_email_confirmation/mailer_view.ex
+++ lib/feeddev_web/views/pow_email_confirmation/mailer_view.ex
@@ -1,4 +1,4 @@
-defmodule Feeddev.PowEmailConfirmation.MailerView do
+defmodule FeeddevWeb.PowEmailConfirmation.MailerView do
   use FeeddevWeb, :mailer_view
 
   def subject(:email_confirmation, _assigns), do: "Confirm your email address"
